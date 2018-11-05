ec800122f51f0e83d0c3b489085d905200f29c61
Hannes Lowette <lowette.hannes@gmail.com>
2018-11-05 11:12:15 +0100

    Re-add the Marbles plugin

---
+++ Axxes.ToyCollector.Plugins.Marbles/Controllers/MarblesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Axxes.ToyCollector.Plugins.Marbles.Controllers
+{
+    public class MarblesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
