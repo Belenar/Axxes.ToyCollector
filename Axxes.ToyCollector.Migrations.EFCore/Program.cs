e7891c0fd47d8320d3f4266d7adfc74861b57e3f
Hannes Lowette <lowette.hannes@gmail.com>
2018-11-05 19:01:01 +0100

    Add a Migration project

---
+++ Axxes.ToyCollector.Migrations.EFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Axxes.ToyCollector.Migrations.EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
